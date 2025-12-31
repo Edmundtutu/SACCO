e6362c5e43098a1fe0a65b5e141a361472dc145f
Edmundtutu <2021bse180@std.must.ac.ug>
2025-12-31 08:05:00 +0300

    Update service worker revision for index.html caching

--- pwa/dev-dist/sw.js
+++ pwa/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gsis4gasu5o"
+    "revision": "0.c14uapent2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
